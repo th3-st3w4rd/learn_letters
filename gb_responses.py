b2f259352ca12b35a3fb6bb2fcfff7bba61912a1
th3-st3w4rd <ChristianS.Gren@gmail.com>
2025-02-05 17:47:57 -0500

    Added variety to (in)correct responses.

---
+++ gb_responses.py
@@ -0,0 +1,17 @@
+victories =[
+    "Booya! You got it correct!",
+    "Wowza! Good job!",
+    "Correct! I knew you could do it!",
+    "You rock!",
+    "Let's GOOOOOOO! Woohoo!",
+    "How did you know? Amazing!",
+    ]
+
+defeats = [
+    "Oh no! Those letters didn't match...",
+    "You'll do better next time!",
+    "Keep practicing, you'll get the hang of it!",
+    "Aww shux, I'm sorry. That didn't match.",
+    "So close! Try again!",
+    "That didn't match, but I know you can do it!",
+    ]
--- learn_letters.py
+++ learn_letters.py
@@ -3,6 +3,8 @@ Helps kindergarteners to learn letters and typing.
 """
 from time import sleep
 from random import choice
+from gb_responses import victories, defeats
+
 alphabet = [
     "a",
     "b",
@@ -59,18 +61,39 @@ capitol_alphabet = [
     "Y",
     "Z"
     ]
+intro = "Hello!  My name is G34r-B0lt! GB for short."
+goals = "My goal is to help you practice your letters, numbers, symbols, and typing!"
+instructions = "I'll show a letter to the screen and you try to match it!"
+how_to_close = "If at any point you want to stop playing, enter '--'"
+salutation = "Happy learning!\n"
 combined_alphabet = alphabet + capitol_alphabet
+#rando_lett = choice(combined_alphabet)
 def play():
+    user_correct = 0
+    user_incorrect = 0 
     play = True
-    print("Type '--' to quit the game!\n")
+    print(intro)
+    sleep(1)
+    print(goals)
+    sleep(1)
+    print(instructions)
+    sleep(.5)
+    print(how_to_close)
+    sleep(.5)
+    print(salutation)
+    sleep(.25)
     while play:
         rando_lett = choice(combined_alphabet)
-        print(f"Computer sees: {rando_lett}")
+        print(f"GB: Try to match: '{rando_lett}'")
         user_input = input("What letter do you see? ")
         if rando_lett == user_input:
-            print("Booya!! You got it correct!  Good Job!")
+            print("CORRECT")
+            print(choice(victories))
+            user_correct += 1
         elif rando_lett != user_input and user_input != "--":
-            print("Oh no! Those letters don't match!")
+            print("NOT CORRECT")
+            print(choice(defeats))
+            user_incorrect += 1
         elif user_input == "--":
             print("Quitting the game!")
             sleep(.5)
@@ -79,7 +102,7 @@ def play():
             play = False
         else:
             print("an error has occured")
-        print("")
+        print(f"CORRECT: {user_correct} INCORRECT: {user_incorrect}\n")
         sleep(.5)
 
 if __name__ == "__main__":
