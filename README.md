3416addb495aa07708d5172f99642a10409b2c2e
th3-st3w4rd <ChristianS.Gren@gmail.com>
2025-02-05 16:59:07 -0500

    Added improvements on readme and added capitol letters.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # learn_letters
+## Imporovements
+- [ ] Create a narrorator, Gear Bolt
+- [ ] Add more cnograts satements.
+- [ ] Add more failure statements.
+- [ ] Add input validation for quantity of characters.
+- [ ] Add options to select lower, upper, numbers, symbols and any combo.
--- learn_letters.py
+++ learn_letters.py
@@ -31,23 +31,56 @@ alphabet = [
     "y",
     "z"
     ]
-play = True
-print("Type '--' to quit the game!\n")
-while play:
-    rando_lett = choice(alphabet)
-    print(f"Computer sees: {rando_lett}")
-    user_input = input("What letter do you see? ")
-    if rando_lett == user_input:
-        print("Booya!! You got it correct!  Good Job!")
-    elif rando_lett != user_input and user_input != "--":
-        print("Oh no! Those letters don't match!")
-    elif user_input == "--":
-        print("Quitting the game!")
+capitol_alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z"
+    ]
+combined_alphabet = alphabet + capitol_alphabet
+def play():
+    play = True
+    print("Type '--' to quit the game!\n")
+    while play:
+        rando_lett = choice(combined_alphabet)
+        print(f"Computer sees: {rando_lett}")
+        user_input = input("What letter do you see? ")
+        if rando_lett == user_input:
+            print("Booya!! You got it correct!  Good Job!")
+        elif rando_lett != user_input and user_input != "--":
+            print("Oh no! Those letters don't match!")
+        elif user_input == "--":
+            print("Quitting the game!")
+            sleep(.5)
+            print("Thank you for playing!")
+            sleep(1)
+            play = False
+        else:
+            print("an error has occured")
+        print("")
         sleep(.5)
-        print("Thank you for playing!")
-        sleep(1)
-        play = False
-    else:
-        print("an error has occured")
-    print("")
-    sleep(.5)
+
+if __name__ == "__main__":
+    play()
